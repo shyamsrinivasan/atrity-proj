cf82d7d7b1ce863edb83fa1e988afae27fa73631
Shyam Srinivasan <shyamsrinivasan@users.noreply.github.com>
2023-08-09 18:44:08 +0530

    working redirect after signup

--- mysite/userapp/urls.py
+++ mysite/userapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 
 from .views import SignupView
 
-urlpatterns = [path('sign-up/', SignupView.as_view(), name='user-signup'),
+# app_name = 'userapp'
+urlpatterns = [path('sign-up/', SignupView.as_view(), name='user-sign-up'),
                ]
--- mysite/userapp/views.py
+++ mysite/userapp/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.http import HttpResponseRedirect, JsonResponse
+
+from .forms import NewUserForm
 
 
 # Create your views here.
 class SignupView(generic.CreateView):
     """view class for user sign up"""
-    form_class = UserCreationForm
+    form_class = NewUserForm
     success_url = reverse_lazy('login')
     template_name = 'userapp/registration/signup.html'
+
+    def __init__(self, *args, **kwargs):
+        super(SignupView, self).__init__(*args, **kwargs)
+
+    def form_valid(self, form):
+        """overrides parent class method"""
+        # call super to execute parent class method
+        self.object = form.save(commit=False)
+        self.object.save()
+        # response = super(SignupView, self).form_valid(form)
+        # if self.request.accepts("text/html"):
+        #     return response
+        # else:
+        #     return JsonResponse({'pk': self.object.pk})
+        return HttpResponseRedirect(self.get_success_url())
+
+
