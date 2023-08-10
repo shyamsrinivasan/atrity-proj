b4de3fb265fbda30c2182951f16665607d0834f1
Shyam Srinivasan <shyamsrinivasan@users.noreply.github.com>
2023-08-10 13:36:17 +0530

    add LOGIN_REDIRECT_URL

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# successful login redirect
+LOGIN_REDIRECT_URL = 'home'
+# logout redirect
+LOGOUT_REDIRECT_URL = 'home'
